f766761ddc0d0a81f422e2f70bedab6677d4b9a3
Will Spaur <mitchellspaur@gmail.com>
2021-04-28 15:34:57 -0700

    finished node project

--- List/index.js
+++ List/index.js
@@ -1,16 +1,57 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const util = require('util');
+const chalk = require('chalk');
+const path = require('path');
 
-fs.readdir(process.cwd(), (err, filenames) => {
-  // EITHER
-  // err === an error object, which means something went wrong
-  // OR
-  // err === null, which means everything is OK
+// const lstat = util.promisify(fs.lstat);
 
+const { lstat } = fs.promises;
+
+const targetDir = process.argv[2] ||  process.cwd();
+
+fs.readdir(targetDir, async (err, filenames) => {
   if (err) {
-    // error handling code here
     console.log(err);
   }
-  console.log(filenames);
+
+  const statPromises = filenames.map(filename => {
+    return lstat(path.join(targetDir, filename));
+  });
+
+  const allStats = await Promise.all(statPromises);
+
+  for (let stats of allStats) {
+    const index = allStats.indexOf(stats);
+
+    if (stats.isFile()){
+      console.log(filenames[index]);
+    } else {
+      console.log(chalk.bold(filenames[index]));
+    }
+  }
+
+  // for (let filename of filenames) {
+  //   try {
+  //   const stats = await lstat(filename);
+
+  //   console.log(filename, stats.isFile());
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
+
+  // const lstat = (filename) => {
+  //   return new Promise((resolve, reject) => {
+  //     fs.lstat(filename, (err, stats) => {
+  //       if (err) {
+  //         reject(err);
+  //       }
+
+  //       resolve(stats);
+  //     })
+  //   })
+  // }
+
 });
